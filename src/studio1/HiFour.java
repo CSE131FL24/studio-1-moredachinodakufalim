9bafc6dc637600987ef1bf3d049d2740b36ddfcd
joon <joon@localhost>
2024-09-04 15:09:22 -0500

    HiFour Finished

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -20,6 +20,10 @@ public class HiFour {
 		//
 		// Say hello to the names in s0 through s3.
 		//
+		System.out.println("Hello, " + s0);
+		System.out.println("Hello, " + s1);
+		System.out.println("Hello, " + s2);
+		System.out.println("Hello, " + s3);
 
 	}
 }
